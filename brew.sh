cc8a19a136d48d5fd7778d684ebe77fb877f87f9
Kai KANG <myfavouritekk@gmail.com>
2015-04-08 14:56:45 +0800

    Update brew installation script; add cask script.

--- brew.sh
+++ brew.sh
@@ -34,67 +34,44 @@ brew install bash-completion
 # Install `wget` with IRI support.
 brew install wget --with-iri
 
-# Install RingoJS and Narwhal.
-# Note that the order in which these are installed is important;
-# see http://git.io/brew-narwhal-ringo.
-brew install ringojs
-brew install narwhal
-
 # Install more recent versions of some OS X tools.
 brew install vim --override-system-vi
 brew install homebrew/dupes/grep
 brew install homebrew/dupes/openssh
 brew install homebrew/dupes/screen
-brew install homebrew/php/php55 --with-gmp
-
-# Install font tools.
-brew tap bramstein/webfonttools
-brew install sfnt2woff
-brew install sfnt2woff-zopfli
-brew install woff2
-
-# Install some CTF tools; see https://github.com/ctfs/write-ups.
-brew install aircrack-ng
-brew install bfg
-brew install binutils
-brew install binwalk
-brew install cifer
-brew install dex2jar
-brew install dns2tcp
-brew install fcrackzip
-brew install foremost
-brew install hashpump
-brew install hydra
-brew install john
-brew install knock
-brew install nmap
-brew install pngcheck
-brew install socat
-brew install sqlmap
-brew install tcpflow
-brew install tcpreplay
-brew install tcptrace
-brew install ucspi-tcp # `tcpserver` etc.
-brew install xpdf
-brew install xz
 
 # Install other useful binaries.
 brew install ack
-#brew install exiv2
+brew install autojump
+brew install brew-cask
+brew install capnp
+brew install cheat
+brew install cloc
+brew install cmake
+brew install curl
+brew install ffmpeg
+brew install gawk
+brew install gcc
+brew install gist
 brew install git
+brew install git-flow
+brew install gnutls
+brew install go
+brew install highlight
+brew install hub
 brew install imagemagick --with-webp
-brew install lua
-brew install lynx
-brew install p7zip
-brew install pigz
-brew install pv
+brew install macvim
+brew install mplayer
+brew install pandoc
+brew install parallel
+brew install python
 brew install rename
-brew install rhino
-brew install speedtest_cli
+brew install shellcheck
 brew install ssh-copy-id
-brew install tree
-brew install webkit2png
-brew install zopfli
+brew install terminal-notifier
+brew install vim
+brew install watch
+brew install z
 
 # Remove outdated versions from the cellar.
 brew cleanup
---
+++ cask.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+# Install Mac Applications using Homebrew Cask
+brew cask install alfred
+brew cask install dropbox
+brew cask install eclipse-cpp
+brew cask install evernote
+brew cask install google-chrome
+brew cask install google-hangouts
+brew cask install istat-menus
+brew cask install iterm2
+brew cask install karabiner
+brew cask install papers
+brew cask install qq
+brew cask install seil
+brew cask install sourcetree
+brew cask install sublime-text
+brew cask install thunder
+brew cask install tower
+brew cask install transmit
+brew cask install vlc
+brew cask install wechat
